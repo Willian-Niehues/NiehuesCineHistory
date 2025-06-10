b077f1a279edf9d14a9d92a3e9053502b6e8505d
Willian Niehues <102665552+Willian-Niehues@users.noreply.github.com>
2025-06-10 10:35:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,124 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-  const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
-  
-  botaoDeAcessibilidade.addEventListener('click', function(){
-  botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-  opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Lemon&display=swap');
 
-  const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
-  botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
-  })
-  
-  
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+:root {
+    --laranja-claro: #FF862A;
+    --alto-contraste-fundo: #000000;
+    --alto-contraste-texto: #ffffff;
+    --alto-contraste-link: #ffd700;
+}
 
-    const alternaContraste = document.getElementById('alterna-contreste');
+body {
+    color: white;
+    background: rgb(194, 201, 206);
+    margin: 0px font-family: "Kablammo", system-ui;
+    margin-bottom: 100px;
+}
 
-    let tamanhoAtualFonte = 1;
-    aumentaFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+header {
+    border-bottom: solid 2px rgb(42, 122, 228);
+    padding: 20px;
+    font-size: 32px;
+    color: rgb(109, 149, 156);
+}
 
-    });
+.chamada {
+    background: rgb(84, 90, 95);
+    padding-bottom: 80px;
+    padding-top: 80px;
+    display: flex;
+    justify-content: center;
+}
 
-    diminuiFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+.chamada-texto {
+    margin-right: 10%;
+}
 
-    });
+h1 {
+    font-size: 40px;
+}
 
-    alternaContraste.addEventListener('click', function(){
-        document.body.classList.toggle("alto-contraste")
-    })
+p {
+    font-size: 15px;
+}
 
-});
+img {
+    height: 200px;
+}
 
+.categoria-videos {
+    display: flex;
+    overflow-x: auto;
+    gap: 10px;
+}
 
-ScrollReveal().reveal('#chamada', { delay: 500 });
-ScrollReveal().reveal('#categoria', { delay: 500 });
+.categoria {
+    padding-left: 20px;
+    padding-right: 20px;
+    margin-top: 50px;
+}
+
+.categoria-videos img {
+    opacity: 0.5;
+}
+
+.categoria-videos img:hover {
+    opacity: 1.0;
+    border: 2px solid black;
+}
+
+.categoria h2 {
+    color: rgb(109, 149, 156);
+}
+.menu-acessibilidade{
+    position: fixed;
+    top:2rem;
+    right: 20px;
+    z-index: 1000;
+}
+.rotacao-botao{
+    transform: rotate(-90deg);
+    transform-origin: right center;
+}
+.opcoes-acessibilidade{
+    margin-top: 10px;
+    display: flex;
+    flex-direction: column;
+}
+.opcoes-acessibilidade button{
+    margin-bottom: 5px;
+}
+.apresenta-lista{
+    display: none;
+}
+.alto-contraste{
+background-color: var(--alto-contraste-fundo);
+color: var(--alto-contraste-texto);
+}
+.alto-contraste header,
+.alto-contraste footer,
+.alto-contraste .formulario{
+    background-color: var(--alto-contraste-fundo);
+    color: var(--alto-contraste-texto);
+}
+.alto-contraste .nav-link{
+    color: var (--alto-contraste-link);
+}
+
+.alto-contraste .botao-inicio,
+.alto-contraste .formulario button,
+.alto-contraste .btn-primary{
+    background: var(--alto-contraste-link);
+    color: var(--alto-contraste-fundo);
+}
+
+.alto-contraste #chamada{
+    background: none;
+}
+
+.alto-contraste #categoria{
+    background-color: var(--alto-contraste-fundo) ;
+}
+.alto-contraste .fundo-categoria {
+    background: none;
+}
